5b79f2091148baf7a3d47d09a46620b055ad721f
ShootDawn <72670900+ShootDawn@users.noreply.github.com>
2022-11-29 03:28:53 +0000

    Music fix I hope

--- GameOff2022/Assets/Scripts/EmemyBehaviors/ProximityBomb.cs
+++ GameOff2022/Assets/Scripts/EmemyBehaviors/ProximityBomb.cs
@@ -14,6 +14,8 @@ public class ProximityBomb : MonoBehaviour
     public ParticleSystem explodeParticlesPrefab;
     public LayerMask groundLayers;
     public LineRenderer lineRend;
+    private FMOD.Studio.EventInstance chargeBombSFX;
+
 
     // Start is called before the first frame update
     void Start()
@@ -33,19 +35,37 @@ public class ProximityBomb : MonoBehaviour
             bool playerInView = !Physics2D.Raycast(transform.position, player.position - transform.position, dist, groundLayers);
             if (dist < activeDist && playerInView)
             {
+                if (!IsPlaying(chargeBombSFX))
+                {
+                    chargeBombSFX = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/Enemies/ProximityMine");
+                    chargeBombSFX.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject.transform));
+                    chargeBombSFX.start();
+                    chargeBombSFX.release();
+                }
                 lineRend.SetPosition(1, player.position);
                 anim.SetBool("Charging", true);
                 // Explode
                 if (chargeTimer < 0)
                 {
+                    FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Enemies/Bomb");
                     Instantiate(explodeParticlesPrefab, transform.position, Quaternion.identity);
                     Destroy(gameObject);
+                    if (IsPlaying(chargeBombSFX))
+                    {
+                        chargeBombSFX.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                        chargeBombSFX.release();
+                    }
                 }
                 chargeTimer -= Time.deltaTime;
             }
             // Not in view/range
             else
             {
+                if (IsPlaying(chargeBombSFX))
+                {
+                    chargeBombSFX.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                    chargeBombSFX.release();
+                }
                 lineRend.SetPosition(1, transform.position);
                 anim.SetBool("Charging", false);
                 if (chargeTimer < chargeTime)
@@ -54,5 +74,20 @@ public class ProximityBomb : MonoBehaviour
             //loadIndicator.localScale = Vector3.one * Mathf.Clamp(2.5f * (0.4f - (shotTimer / timeBtwnBursts)), 0, 1) * indicatorScale;
             chargeIndicator.transform.localScale = Vector2.one * Mathf.Clamp(2.5f * (0.4f - (chargeTimer / chargeTime)), 0, 1) * 0.86f;
         }   
+    }
+    bool IsPlaying(FMOD.Studio.EventInstance instance)
+    {
+        FMOD.Studio.PLAYBACK_STATE state;
+        instance.getPlaybackState(out state);
+        return state != FMOD.Studio.PLAYBACK_STATE.STOPPED;
+    }
+    private void OnDestroy()
+    {
+        if (IsPlaying(chargeBombSFX))
+        {
+            chargeBombSFX.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            chargeBombSFX.release();
+        }
+
     }
 }
