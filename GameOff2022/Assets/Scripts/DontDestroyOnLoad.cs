0091d2e6aec1e4e2fc3f54a9e866f52aa9f687cd
ShootDawn <72670900+ShootDawn@users.noreply.github.com>
2022-11-30 03:00:12 +0000

    Music changes

--- GameOff2022/Assets/Scripts/DontDestroyOnLoad.cs
+++ GameOff2022/Assets/Scripts/DontDestroyOnLoad.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroyOnLoad : MonoBehaviour
 {
     public static bool created;
     private FMOD.Studio.EventInstance BGM;
+    private int scene;
     // Start is called before the first frame update
     void Awake()
     {
@@ -21,11 +23,41 @@ public class DontDestroyOnLoad : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
     }
+    private void Update()
+    
+    {
+       if (SceneManager.GetActiveScene().buildIndex == 0) {
+            BGM.setParameterByName("Intensity", 0);
+        } else if (SceneManager.GetActiveScene().buildIndex >= 1 && SceneManager.GetActiveScene().buildIndex < 3)
+        {
+            BGM.setParameterByName("Intensity", 1);
+        }
+        else if (SceneManager.GetActiveScene().buildIndex >= 3 && SceneManager.GetActiveScene().buildIndex < 6)
+        {
+            BGM.setParameterByName("Intensity", 2);
+        }
+        else if (SceneManager.GetActiveScene().buildIndex >= 6 && SceneManager.GetActiveScene().buildIndex < 11)
+        {
+            BGM.setParameterByName("Intensity", 3);
+        }
+        else if (SceneManager.GetActiveScene().buildIndex >= 11 && SceneManager.GetActiveScene().buildIndex < 15)
+        {
+            BGM.setParameterByName("Intensity", 4);
+        }
+        else if (SceneManager.GetActiveScene().buildIndex >= 15 && SceneManager.GetActiveScene().buildIndex <= 17)
+        {
+            BGM.setParameterByName("Intensity", 5);
+        }
+    }
     private void OnDestroy()
     {
         BGM.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
         BGM.release();
+       
+
+
     }
+ 
 
 
 }
